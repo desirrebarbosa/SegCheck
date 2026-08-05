8ae6fe5c489313666993ab8140a44f5a164e6991
Desirre Barbosa <desblssbarbosa@gmail.com>
2026-08-05 18:43:21 +0800

    feat: added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { supabase } from './lib/supabaseClient'
 import { useSession } from './auth/useSession'
 import { ToastProvider } from './components/Toast'
+import { Analytics } from "@vercel/analytics/next"
 import Login from './pages/Login'
 import ProjectsList from './pages/ProjectsList'
 import ProjectLayout from './components/ProjectLayout'
